b2d9d03f3926bc756b32221fd7404fa10ca2ad7d
kingl23 <38876427+kingl23@users.noreply.github.com>
2025-12-22 20:53:52 +0900

    Update worst_offense.py

--- ui/worst_offense.py
+++ ui/worst_offense.py
@@ -1,6 +1,6 @@
 # ui/worst_offense.py
 import streamlit as st
-from app import require_access_or_stop
+from auth import require_access_or_stop
 from data.siege_data import build_worst_offense_list, get_offense_stats_by_defense
 
 
